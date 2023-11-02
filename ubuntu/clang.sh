7af2d81c02c62b9bbc8dd1eae66c3626257e122e
Soren Telfer <soren.telfer@gmail.com>
2023-11-02 11:05:28 -0700

    Added clang-tools to build, modules need scan-deps

--- ubuntu/clang.sh
+++ ubuntu/clang.sh
@@ -21,6 +21,7 @@ apt-get install -y \
     llvm-$CLANG_VER \
     clang-$CLANG_VER \
     clangd-$CLANG_VER \
+    clang-tools-$CLANG_VER \
     libstdc++-${GCC_VER}-dev \
     libasan6
 
@@ -29,5 +30,6 @@ update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$CLANG_VER 50
 update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-$CLANG_VER 50
 update-alternatives --install /usr/bin/llvm-symbolizer llvm-symbolizer /usr/bin/llvm-symbolizer-$CLANG_VER 50
 update-alternatives --install /usr/bin/llvm-addr2line llvm-addr2line /usr/bin/llvm-addr2line-$CLANG_VER 50
+update-alternatives --install /usr/bin/clang-scan-deps clang-scan-deps /usr/bin/clang-scan-deps-$CLANG_VER 50
 
 
